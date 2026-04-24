92069136fb6cd29a18c7c0fa49626a68889e0c63
Harun Ćatić <catic.harun@gmail.com>
2026-04-24 22:01:11 +0200

    LED Edit
    Main LED Edit. By default they were ON. Switched to GPIO Output Level = High

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -217,7 +217,7 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, Red_Pin|Blue_Pin|Green_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, Red_Pin|Blue_Pin|Green_Pin, GPIO_PIN_SET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_RESET);
